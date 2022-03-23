79a248e0ba2dff73bd65f6c7ba2777948e1eaae1
Kamil Posvic <kamil@posvic.com>
2022-03-23 13:10:17 +0100

    Added class Tools and JUnit test class (ready for TDD)

---
+++ test/application/ToolsTest.java
@@ -0,0 +1,14 @@
+package application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ToolsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
