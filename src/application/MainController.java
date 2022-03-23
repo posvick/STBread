540515cf3dfd1fe9d891c9459a3905fa6ca35284
Kamil Posvic <kamil@posvic.com>
2022-03-23 12:42:00 +0100

    Added basic references to controller

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -2,6 +2,20 @@ package application;
 
 import javafx.fxml.FXML;
 
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
+
+import javafx.scene.control.TableView;
+
 public class MainController {
+	@FXML
+	private Button pickFolder;
+	@FXML
+	private TextField folderPath;
+	@FXML
+	private TableView table;
+	@FXML
+	private Button readMovies;
 
 }
