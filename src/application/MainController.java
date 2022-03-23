2a59c8e639d811f0341e0e47013949b8f477d419
Kamil Posvic <kamil@posvic.com>
2022-03-23 13:07:33 +0100

    Added directory chooser on button ... click

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,11 +1,14 @@
 package application;
 
+import java.io.File;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 import javafx.scene.control.Button;
 
 import javafx.scene.control.TextField;
-
+import javafx.stage.DirectoryChooser;
 import javafx.scene.control.TableView;
 
 public class MainController {
@@ -18,4 +21,10 @@ public class MainController {
 	@FXML
 	private Button readMovies;
 
+	public void pickFolderClick(ActionEvent event) {
+		DirectoryChooser dc = new DirectoryChooser();
+		File selectedFolder = dc.showDialog(null);
+		folderPath.setText(selectedFolder.getPath());
+		
+	}
 }
